6896a88d37ac16f5ed5f0756f76416ff22b87f89
coderhombus <187402134+coderhombus@users.noreply.github.com>
2026-05-02 23:50:10 +0100

    log and xmake additions
    log to console as well as file
    new xmake

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,14 @@ SKSE_PLUGIN_LOAD(const SKSE::LoadInterface* a_skse)
 {
 	SKSE::Init(a_skse);
 
+  // logs to plugin_name.log
 	logs::info("Hello World!");
+
+  // logs to skyrim console
+  SKSE::GetMessagingInterface()->RegisterListener([](SKSE::MessagingInterface::Message *message) {
+      if (message->type == SKSE::MessagingInterface::kDataLoaded)
+          RE::ConsoleLog::GetSingleton()->Print("Hello, world!");
+  });
 
 	return true;
 }
--- xmake.lua
+++ xmake.lua
@@ -2,37 +2,47 @@
 -- CONFIG
 ----------------------------------------
 
--- set mod install directory
-local mod_dir = "D:/Games/Mods/SE/MO2/mods/Dev"
-local project_name = "commonlibsse-ng-template"
+-- paths
+local project_name  = "first-skse"
 
--- include subprojects
+local mod_folder    = "Dev"
+local dir_mo2       = "D:/Games/Mods/SE/MO2/mods"
+local dir_mod       = path.join(dir_mo2, mod_folder)
+
+-- launch.json example:
+-- .\\MO2\\ModOrganizer.exe -p "dev" SKSE
+
+----------------------------------------
+-- PROJECT SETUP
+----------------------------------------
+
 includes("lib/commonlibsse-ng")
 
--- set project constants
 set_project(project_name)
 set_version("0.0.0")
 set_license("GPL-3.0")
 set_languages("c++23")
 set_warnings("allextra")
 
--- add common rules
 add_rules("mode.debug", "mode.releasedbg")
 add_rules("plugin.vsxmake.autoupdate")
 
--- define targets
+----------------------------------------
+-- TARGET
+----------------------------------------
+
 target(project_name)
+    set_kind("shared")
+
     add_rules("commonlibsse-ng.plugin", {
-        name = project_name,
-        author = "ASX",
+        name        = project_name,
+        author      = "ASX",
         description = "SKSE Plugin"
     })
 
-    -- add src files
     add_files("src/**.cpp")
     add_headerfiles("src/**.h")
     add_includedirs("src")
     set_pcxxheader("src/pch.h")
 
-    -- set mod directory
-    set_installdir(mod_dir)
+    set_installdir(dir_mod)
