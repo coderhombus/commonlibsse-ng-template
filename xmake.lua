b5fc67fb78b3d99cf56091b2c592f9d0d152cc21
coderhombus <187402134+coderhombus@users.noreply.github.com>
2026-06-02 07:02:27 +0100

    Update xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -6,6 +6,7 @@
 local project_name  = "template"
 local mod_folder    = "Dev"
 local dir_mo2       = "D:/Games/Mods/SE/MO2/mods"
+local clib_ng      = "../../_lib/commonlibsse-ng"
 
 local dir_mod       = path.join(dir_mo2, mod_folder)
 
@@ -16,7 +17,7 @@ local dir_mod       = path.join(dir_mo2, mod_folder)
 -- PROJECT SETUP
 ----------------------------------------
 -- path to commonlibsse-ng, and other includes
-includes("../../_lib/commonlibsse-ng")
+includes(clib_ng)
 
 set_project(project_name)
 set_version("0.0.0")
